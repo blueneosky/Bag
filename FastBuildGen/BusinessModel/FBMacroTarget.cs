128c226cc3fb117db031e4c75c954e363b9e541f
prioulj <jean.marie.prioul@hager.fr>
2014-08-25 16:18:26 +0200

    tt le monde peut faire des erreur :/

---
+++ FastBuildGen/BusinessModel/FBMacroTarget.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FastBuildGen.BusinessModel
+{
+    internal class FBMacroTarget : MSBuildBaseTarget
+    {
+        public FBMacroTarget(Guid id)
+            : base(id)
+        {
+        }
+
+#warning TODO ALPHA ALPHA point
+    }
+}
---
+++ FastBuildGen/BusinessModel/FBTarget.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FastBuildGen.BusinessModel
+{
+    class FBTarget : MSBuildBaseTarget
+    {
+        public FBTarget(Guid id)
+            :base(id)
+        {
+
+        }
+#warning TODO ALPHA ALPHA point
+    }
+}
