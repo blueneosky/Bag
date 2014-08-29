ad88d0b56e4b2c90e44e105777ffefe982ded303
Jean-Marie PRIOUl <jm.prioul@gmail.com>
2014-08-29 23:56:37 +0200

    add ApplicationModel and it's controller

---
+++ FastBuildGen/FastBuildGen/BusinessModel/ApplicationController.cs
@@ -0,0 +1,24 @@
+﻿namespace FastBuildGen.BusinessModel
+{
+    internal class ApplicationController
+    {
+        private readonly ApplicationModel _model;
+
+        public ApplicationController(ApplicationModel model)
+        {
+            _model = model;
+        }
+
+        internal void SaveFastBuildConfig(string configFilePath)
+        {
+#warning TODO DELTA point - not necessary to keep it
+            throw new System.NotImplementedException();
+        }
+
+        internal bool LoadFastBuildConfig(string configFilePath)
+        {
+#warning TODO DELTA point - not necessary to keep it
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ FastBuildGen/FastBuildGen/BusinessModel/ApplicationModel.cs
@@ -0,0 +1,58 @@
+﻿using System.ComponentModel;
+using FastBuildGen.Common;
+
+namespace FastBuildGen.BusinessModel
+{
+    internal class ApplicationModel : INotifyPropertyChanged
+    {
+        private FBModel _fbModel;
+
+        public ApplicationModel()
+        {
+            _fbModel = null;
+        }
+
+       public FBModel FBModel
+        {
+            get { return _fbModel; }
+            set
+            {
+                if (_fbModel != null)
+                {
+                    _fbModel.PropertyChanged -= _fbModel_PropertyChanged;
+                }
+                _fbModel = value;
+                if (_fbModel != null)
+                {
+                    _fbModel.PropertyChanged += _fbModel_PropertyChanged;
+                }
+#warning TODO BETA point - add fire event
+            }
+        }
+
+       void _fbModel_PropertyChanged(object sender, PropertyChangedEventArgs e)
+       {
+#warning TODO DELTA point - add implementation
+           throw new System.NotImplementedException();
+       }
+       public bool DataChanged
+       {
+           get
+           {
+#warning TODO DELTA point - add DataChanged management here
+               return false; // _fastBuildModel.DataChanged;
+           }
+       }
+        
+        #region INotifyPropertyChanged
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            PropertyChanged.Notify(sender, e);
+        }
+
+        #endregion INotifyPropertyChanged
+    }
+}
