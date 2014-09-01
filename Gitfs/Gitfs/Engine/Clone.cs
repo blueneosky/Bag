68016d553f5072376ee50210271a2d45510a56ef
blueneosky <blueneosky@gmail.com>
2014-09-01 14:43:00 +0200

    move Output in Util namespace

--- Gitfs/Gitfs/Engine/Clone.cs
+++ Gitfs/Gitfs/Engine/Clone.cs
@@ -6,6 +6,7 @@ using System.Linq;
 using System.Text;
 using Microsoft.TeamFoundation.Client;
 using Microsoft.TeamFoundation.VersionControl.Client;
+using Gitfs.Util;
 
 namespace Gitfs.Engine
 {
--- Gitfs/Gitfs/Engine/Help.cs
+++ Gitfs/Gitfs/Engine/Help.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Gitfs.Util;
 
 namespace Gitfs.Engine
 {
