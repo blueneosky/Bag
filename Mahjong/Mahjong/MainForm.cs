f552d0c654f00c3ac0f518917c56bd07b79f46af
blueneosky <blueneosky@gmail.com>
2015-03-27 16:00:15 +0100

    base of Tiles (graphic extraction ) + basic viewer

---
+++ Mahjong/Mahjong/MainForm.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Mahjong
+{
+    public partial class MainForm : Form
+    {
+
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            TileSet tileSet = TileSetFactory.TileSet;
+
+            hScrollBar1.Minimum = 0;
+            hScrollBar1.Maximum = tileSet.TileByTileTypes.Count - 1;
+            hScrollBar1.Value = 1;
+            hScrollBar1.Value = 0;
+        }
+
+        private void hScrollBar1_ValueChanged(object sender, EventArgs e)
+        {
+            TileSet tileSet = TileSetFactory.TileSet;
+            int index = hScrollBar1.Value;
+            Tile tile = tileSet.TileByTileTypes.Values.Skip(index).FirstOrDefault();
+
+            if (tile == null)
+            {
+                label1.Text = "#errror";
+                pictureBox1.Image = null;
+                return;
+            }
+
+            label1.Text = index+" : "+ tile.TileType.ToString();
+            pictureBox1.Image = tile.Image;
+        }
+
+
+    }
+}
---
+++ Mahjong/Mahjong/TileSet.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mahjong
+{
+    internal class TileSet : IDisposable
+    {
+        private IReadOnlyDictionary<EnumTileType, Tile> _tileByTileTypes;
+        private IReadOnlyDictionary<EnumTileFamily, IReadOnlyDictionary<EnumTileSubNumber, Tile>> _tileByTileSubTypeByTileFamilies;
+
+        public TileSet(IEnumerable<Tile> tiles)
+        {
+            tiles = tiles.Execute();
+
+            _tileByTileTypes = tiles.ToReadOnlyDictionary(t => t.TileType);
+            _tileByTileSubTypeByTileFamilies = tiles
+                .GroupBy(t => t.TileFamily)
+                .ToReadOnlyDictionary(
+                    grp => grp.Key
+                    , grp => grp.ToReadOnlyDictionary(t => t.TileSubNumber)
+                );
+        }
+
+        public IReadOnlyDictionary<EnumTileType, Tile> TileByTileTypes
+        {
+            get { return _tileByTileTypes; }
+        }
+
+        public IReadOnlyDictionary<EnumTileFamily, IReadOnlyDictionary<EnumTileSubNumber, Tile>> TileByTileSubTypeByTileFamilies
+        {
+            get { return _tileByTileSubTypeByTileFamilies; }
+        }
+
+        #region IDisposable Members
+
+        public void Dispose()
+        {
+            Dispose(true);
+        }
+
+        private bool _isDisposed = false;
+
+        protected virtual void Dispose(bool disposed)
+        {
+            if (_isDisposed)
+                return;
+            _isDisposed = true;
+
+            if (_tileByTileTypes != null)
+            {
+                _tileByTileTypes.Values
+                    .Foreach(t => t.Dispose());
+                _tileByTileTypes = null;
+            }
+
+            if (_tileByTileSubTypeByTileFamilies != null)
+            {
+                _tileByTileSubTypeByTileFamilies.Values
+                    .SelectMany(v => v.Values)
+                    .Foreach(t => t.Dispose());
+                _tileByTileSubTypeByTileFamilies = null;
+            }
+        }
+
+        #endregion IDisposable Members
+    }
+}
