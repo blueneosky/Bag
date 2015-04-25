392655e0ad95c32fe753a04c535d5bfb52dd7cea
Jean-Marie PRIOUL <jm.prioul@gmail.com>
2015-04-25 12:53:43 +0200

    refactoring + complete interface => all done

---
+++ Mahjong/Mahjong/Extensions/EventHandlerExtension.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+
+namespace System
+{
+    public static class EventHandlerExtension
+    {
+        public static void Notify(this EventHandler manager, object sender, EventArgs e)
+        {
+            if (manager != null)
+                manager(sender, e);
+        }
+
+        public static void Notify(this PropertyChangedEventHandler manager, object sender, PropertyChangedEventArgs e)
+        {
+            if (manager != null)
+                manager(sender, e);
+        }
+    }
+}
